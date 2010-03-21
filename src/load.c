74fbf93b8164dd4545dcc9531d0407cf2bd8b62e
Vladimir Fedin <vladfedin@yandex.ru>
2010-03-21 21:50:59 +0300

    load.c: string loading fixes, length checks, other minor issues.

--- src/load.c
+++ src/load.c
@@ -53,64 +53,132 @@ int luaamf_load(
 {
   unsigned char type;
   amf_LoadState ls;
-  int result = LUAAMF_ESUCCESS;
+  int result = LUAAMF_EFAILURE;
   int base = lua_gettop(L);
   ls_init(&ls, data, len);
-  if (!ls_good(&ls)) return LUAAMF_EBADDATA;
-  type = ls_readbyte(&ls);
-
-  switch (type)
+  if (!ls_good(&ls)) 
   {
-  case LUAAMF_NULL_AMF:
-    lua_pushnil(L);
-    break;
-
-  case LUAAMF_FALSE_AMF:
-    lua_pushboolean(L, 0);
-    break;
-
-  case LUAAMF_TRUE_AMF:
-    lua_pushboolean(L, 1);
-    break;
-
-  case LUAAMF_INT_AMF:
-    {
-      int curr_value;
-      lua_Number value;
-      result = decode_int(ls.pos, &curr_value);
-      value = curr_value;
-      if (result == LUAAMF_ESUCCESS)
-      {
-        lua_pushnumber(L, value);
-      }
-    }
-    break;
-
-  case LUAAMF_DOUBLE_AMF:
-    {
-      double c_value;
-      lua_Number value;
-      result = decode_double(ls.pos, &c_value);
-      value = c_value;
-      if (result == LUAAMF_ESUCCESS)
-      {
-        lua_pushnumber(L, value);
-      }
-    }
-    break;
-
-  case LUAAMF_STRING_AMF:
-    {
-      int offset = ls.unread > 256 ? 2 : 1;
-      /* TODO: think of long strings and bit masks */
-      lua_pushlstring(L, (const char*)(ls.pos + offset), ls.unread - offset);
-      result = LUAAMF_ESUCCESS;
-    }
-    break;
-
-  default:
     result = LUAAMF_EBADDATA;
-    break;
+  }
+  else 
+  {
+    type = ls_readbyte(&ls);
+    switch (type)
+    {
+    case LUAAMF_NULL_AMF:
+      if(len >= 2)
+      {
+        result = LUAAMF_ETAILEFT;
+        break;
+      }
+      lua_pushnil(L);
+      result = LUAAMF_ESUCCESS;
+      break;
+  
+    case LUAAMF_FALSE_AMF:
+      if(len >= 2)
+      {
+        result = LUAAMF_ETAILEFT;
+        break;
+      }
+      lua_pushboolean(L, 0);
+      result = LUAAMF_ESUCCESS;     
+      break;
+  
+    case LUAAMF_TRUE_AMF:
+      if(len >= 2)
+      {
+        result = LUAAMF_ETAILEFT;
+        break;
+      }
+      lua_pushboolean(L, 1);
+      result = LUAAMF_ESUCCESS;   
+      break;
+  
+    case LUAAMF_INT_AMF:
+      if(len > 5)
+      {
+        result = LUAAMF_ETAILEFT;
+      }
+      else
+      {
+        int curr_value;
+        lua_Number value;
+        result = decode_int(ls.pos, &curr_value);
+        value = curr_value;
+        if (result == LUAAMF_ESUCCESS)
+        {
+          lua_pushnumber(L, value);
+        }
+      }
+      break;
+  
+    case LUAAMF_DOUBLE_AMF:
+      if(len > 9)
+      {
+        result = LUAAMF_ETAILEFT;
+        break;
+      }
+      else
+      {     
+        double c_value;
+        lua_Number value;
+        result = decode_double(ls.pos, &c_value);
+        value = c_value;
+        if (result == LUAAMF_ESUCCESS)
+        {
+          lua_pushnumber(L, value);
+        }
+      }
+      break;
+  
+    case LUAAMF_STRING_AMF:
+      {    
+        int value = 0;
+        int byte_cnt = 0;
+        char byte;
+        if (ls.pos == NULL) return LUAAMF_EBADDATA;
+        byte = ls.pos[0];
+
+        /* If 0x80 is set, int includes the next byte, up to 4 total bytes */
+        while ((byte & 0x80) && (byte_cnt < 3)) {
+            value <<= 7;
+            value |= byte & 0x7F;
+            byte = ls.pos[byte_cnt + 1];
+            byte_cnt++;
+        }
+     
+        /* shift bits in last byte */
+        if (byte_cnt < 3) {
+            /* shift by 7, since the 1st bit is reserved for next byte flag */
+            value <<= 7;
+            value |= byte & 0x7F;
+        } else {
+            /* shift by 8, since no further bytes are
+               possible and 1st bit is not used for flag. */
+            value <<= 8;
+            value |= byte & 0xff;
+        }
+     
+        /* Move sign bit, since we're converting 29bit->32bit */
+        if (value & 0x10000000) {
+            value -= 0x20000000;
+        }
+        if(value != ((ls.unread  - byte_cnt) * 2 - 1))
+        {
+          result = LUAAMF_EBADSIZE;
+        }
+        else
+        {
+          lua_pushlstring(L, (const char*)(ls.pos + byte_cnt + 1), ls.unread - byte_cnt - 1);
+          result = LUAAMF_ESUCCESS;
+        }
+      }
+      break;
+    default:
+      result = LUAAMF_EBADTYPE;
+      break;
+    }
   }
 
   if (result == LUAAMF_ESUCCESS)
@@ -122,6 +190,10 @@ int luaamf_load(
     lua_settop(L, base); /* Discard intermediate results */
     switch (result)
     {
+    case LUAAMF_EBADTYPE:
+      lua_pushliteral(L, "can't load: bad data type");
+      break;
+      
     case LUAAMF_EBADDATA:
       lua_pushliteral(L, "can't load: corrupt data");
       break;
